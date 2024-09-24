e5832984bd0bccb010e455af1f7f148715127b1f
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-09-24 21:47:18 +0100

    Add mongodb connection

---
+++ backend/models/product.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = 5500;
@@ -8,6 +12,11 @@ app.get("/", (req, res) => {
     res.send("Server ready")
 });
 
+app.get("/products", (req, res) => {
+    res.send("products ready")
+});
+
 app.listen(PORT, () => {
-    console.log(`Server start at http://localhost:${PORT}`);    
+    connectDB();
+    console.log(`Server start at http://localhost:${PORT}`);
 });
