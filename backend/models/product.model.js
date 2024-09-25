67e9ed85f8f60dbe7fd60d48b4e876b8270f1426
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-09-25 17:30:32 +0100

    Endpoints create/getall/delete products

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -14,5 +14,9 @@ const productSchema = new mongoose.Schema({
         required: true
     }
 },{
-    timestamps: true
+    timestamps: true // createdAt, updatedAt
 });
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
