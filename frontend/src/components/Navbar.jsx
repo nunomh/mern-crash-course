5d82ec9873683b7b16e39f48d94c46f67ae68d1f
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-09-27 12:12:09 +0100

    Create navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Button, Container, Flex, HStack, Link, Text, useColorMode } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom'
+
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { IoMoon } from 'react-icons/io5';
+import { LuSun } from 'react-icons/lu';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+        <Container maxW={"1140px"} px={4} >
+            <Flex h={16} alignItems={"center"} justifyContent={"space-between"} flexDir={{ base: "column", sm: "row" }} >
+                <Text
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip='text'
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight='bold'
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                >
+                    <Link as={ReactRouterLink} to={"/"}>Product Store</Link>
+                </Text>
+
+                <HStack spacing={2} alignItems={"center"}>
+                    <Link as={ReactRouterLink} to={"/create"}>
+                        <Button>
+                            <PlusSquareIcon fontSize={20} />
+                        </Button>
+                    </Link>
+                    <Button onClick={toggleColorMode}>
+                        {colorMode === 'light' ? <IoMoon /> : <LuSun size="20" />}
+                    </Button>
+                </HStack>
+
+            </Flex>
+        </Container>
+    )
 }
 
 export default Navbar
