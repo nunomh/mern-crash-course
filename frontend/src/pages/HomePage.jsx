c934acadefa47e2a85fde09d9a900fdbaf5e440c
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-09-29 21:29:17 +0100

    dev HomePage products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import { Container, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { useEffect } from 'react';
+import { Container, VStack, Text, Link, SimpleGrid } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { useProductStore } from '../store/products';
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => { fetchProducts() }, [fetchProducts]);
+  console.log("products: ", products);
+
   return (
     <Container maxW={"container.xl"} py={12}>
       <VStack spacing={8}>
-        
+        <Text
+          fontSize={30}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.600)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products
+        </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+
+        </SimpleGrid>
+
+        <Text fontSize={"xl"} textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
+          No Products Found - {" "}
+          <Link as={ReactRouterLink} to={"/create"}>
+            <Text as='span' color="blue.500" _hover={{ textDecoration: "underline" }}>
+              Create a Product
+            </Text>
+          </Link>
+        </Text>
       </VStack>
     </Container>
   )
--- frontend/src/store/products.js
+++ frontend/src/store/products.js
@@ -16,9 +16,14 @@ export const useProductStore = create((set) => ({
         });
         const data = await res.json();
         set((state) => ({
-            products:  [...state.products, data.data]
+            products: [...state.products, data.data]
         }))
         return { success: true, message: "Product created successfully." }
 
+    },
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data });
     }
 }));
